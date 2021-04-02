8fed53e361dae118e9a975458ded967282041036
Peter Harrison <blackriver@simiya.com>
2021-04-02 06:52:26 -0700

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,72 +1,73 @@
 module.exports = {
-    someSidebar: {
-        "Getting Started": ["about-talawa", "about"],
-        Applications: ["applications/architecture-overview", "applications/api-overview", "applications/app-overview", "applications/admin-overview"],
-        'Desired Features': [{
-            Installation: [
-                "features/installation/installation-overview",
-            ],
-            'Chat and Messaging': [
-                "features/messaging-and-chat/messaging-and-chat-overview",
-                "features/messaging-and-chat/message-chat",
-                "features/messaging-and-chat/message-messaging",
-                "features/messaging-and-chat/message-voice-messaging",
-                "features/messaging-and-chat/message-notifications",
-            ],
-            'Admin Capabilities': [
-                "features/admin-role/admin-role-overview",
-                "features/admin-role/admin-messaging",
-                "features/admin-role/admin-manage-members",
-                "features/admin-role/admin-newsfeed"
-            ],
-            'Coordinator Capabilities': [
-                "features/coordinator-role/coordinator-role-overview",
-            ],
-            Events: [
-                "features/events/event-overview",
-                'features/events/event-signup',
-                'features/events/event-grouping',
-                'features/events/event-roles',
-                'features/events/event-attendee-roster',
-                'features/events/event-paid-donation'
-            ],
-            Members: [
-                "features/members/members-overview",
-                'features/members/members-access',
-                'features/members/members-roles',
-                'features/members/members-preapprovals',
-                'features/members/members-registration',
-                'features/members/members-banning',
-                'features/members/members-recovery'
-            ],
-            Donations: [
+  someSidebar: {
+    "Getting Started": ["about-talawa", "about"],
+    Applications: [ "applications/architecture-overview", "applications/api-overview", "applications/app-overview", "applications/admin-overview" ],
+    'Desired Features': [{
+      Installation: [
+        "features/installation/installation-overview",
+      ],
+      'Chat and Messaging': [
+        "features/messaging-and-chat/messaging-and-chat-overview",
+        "features/messaging-and-chat/message-chat",
+        "features/messaging-and-chat/message-messaging",
+        "features/messaging-and-chat/message-voice-messaging",
+        "features/messaging-and-chat/message-notifications",
+      ],
+      'Admin Capabilities': [
+        "features/admin-role/admin-role-overview",
+        "features/admin-role/admin-messaging",
+        "features/admin-role/admin-manage-members",
+        "features/admin-role/admin-newsfeed"
+      ],
+      'Coordinator Capabilities': [
+        "features/coordinator-role/coordinator-role-overview",
+      ],
+      Events: [
+        "features/events/event-overview",
+        'features/events/event-create-events',
+        'features/events/event-signup',
+        'features/events/event-grouping',
+        'features/events/event-roles',
+        'features/events/event-attendee-roster',
+        'features/events/event-paid-donation'
+      ],
+      Members: [
+        "features/members/members-overview",
+        'features/members/members-access',
+        'features/members/members-roles',
+        'features/members/members-preapprovals',
+        'features/members/members-registration',
+        'features/members/members-banning',
+        'features/members/members-recovery'
+      ],
+      Donations: [
                 "features/donations/donations-overview",
-            ],
-            'News Feed': [
-                "features/news feed/news-feed-overview",
-            ],
-            Notifications: [
-                "features/notifications/notifications-overview",
-            ],
-            'Data Management': [
-                "features/data-management/data-management-overview",
-                "features/data-management/data-management-compression",
-                "features/data-management/data-management-retention",
-                "features/data-management/data-management-storage",
-                "features/data-management/data-management-distribution",
-            ],
-            Organisations: [
-                "features/organisations/organisations-overview",
-                "features/organisations/organisations-onboarding",
-            ],
-            Plugins: [
-                "features/plugins/plugins-overview",
-            ],
-            Misc: [
-                "features/misc/misc-overview",
-                'features/misc/misc-language-support',
-            ]
-        }],
-        GSoC: ['gsoc/gsoc-ideas', 'gsoc/selection-criteria'],
-    },
+              ],
+      'News Feed': [
+        "features/news feed/news-feed-overview",
+      ],
+      Notifications: [
+        "features/notifications/notifications-overview",
+      ],
+      'Data Management': [
+        "features/data-management/data-management-overview",
+        "features/data-management/data-management-compression",
+        "features/data-management/data-management-retention",
+        "features/data-management/data-management-storage",
+        "features/data-management/data-management-distribution",
+      ],
+      Organisations: [
+        "features/organisations/organisations-overview",
+        "features/organisations/organisations-onboarding",
+      ],
+      Plugins: [
+        "features/plugins/plugins-overview",
+      ],
+      Misc: [
+        "features/misc/misc-overview",
+        'features/misc/misc-language-support',
+      ]
+    }],
+    GSoC: [ 'gsoc/gsoc-ideas', 'gsoc/selection-criteria'],
+  },
 };
