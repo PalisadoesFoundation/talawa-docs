9a4479d234a48417ad83c7478ebb66d3ddbb069f
Peter Harrison <peter@colovore.com>
2023-02-27 17:53:27 +0800

    Moved plugins to Admin where they belong

--- docs/developers/talawa-admin/introduction.md
+++ docs/developers/talawa-admin/introduction.md
@@ -3,9 +3,7 @@ id: talawa-admin-introduction
 title: Overview
 ---
 
-### Talawa Admin
-
-#### Talawa is a modular open source project to manage group activities of both non-profit organizations and businesses. `Talawa Admin` part of Talwa Application Suite. It works as an admin dashboard for for `Talawa` .
+`Talawa Admin` is the web based administrative dashboard for the `Talawa` mobile app.
 
 ### Core features:
 
@@ -39,17 +37,3 @@ title: Overview
 
 - Contains `Plugin Store` from which the Admin can decide the features for the `Talawa` Mobile App.
 - Admins can `Install` or `Uninstall` the Plugins and can also see the list of installed plugins separately.
-
-### About Talawa
-
-`talawa` is based on the original `quito` code created by the [Palisadoes Foundation][pfd] as part of its annual Calico Challenge program. Calico provides paid summer internships for Jamaican university students to work on selected open source projects. They are mentored by software professionals and receive stipends based on the completion of predefined milestones. Calico was started in 2015. Visit [The Palisadoes Foundation's website](http://www.palisadoes.org/) for more details on its origin and activities.
-
-### Documentation
-
-- The `talawa-admin` documentation can be found [here](https://docs.talawa.io/talawa-admin).
-- Want to contribute? Look at [Contributing Guide](https://github.com/PalisadoesFoundation/talawa-admin/blob/develop/CONTRIBUTING.md) to get started.
-- Visit the [Talawa-Docs GitHub](https://github.com/PalisadoesFoundation/talawa-docs) to see the code.
-
-## Community
-
-[💬 Join the community on Slack](https://join.slack.com/t/thepalisadoes-dyb6419/shared_invite/zt-1gkkn2kpt-zHhYXpS8cYHNdS~v4o2jWQ)
--- sidebars.js
+++ sidebars.js
@@ -1,122 +1,136 @@
 module.exports = {
   someSidebar: {
-    "Introduction": ["about"],
-    "Technical Overview": [
-      "applications/architecture-overview",
-      "applications/overview",
+    Introduction: ['about'],
+    'Technical Overview': [
+      'applications/architecture-overview',
+      'applications/overview'
     ],
-    "Developer Resources": [
+    'Developer Resources': [
       {
-        Talawa: [
-            "developers/talawa/talawa-introduction",
-        ],
+        Talawa: ['developers/talawa/talawa-introduction']
       },
       {
-        "Talawa API": [
-          "developers/talawa-api/api-overview",
+        'Talawa API': [
+          'developers/talawa-api/api-overview',
           {
-            "Base Constraints": [
-              "developers/talawa-api/constraints/user",
-              "developers/talawa-api/constraints/organization",
-              "developers/talawa-api/constraints/admin",
-            ],
+            'Base Constraints': [
+              'developers/talawa-api/constraints/user',
+              'developers/talawa-api/constraints/organization',
+              'developers/talawa-api/constraints/admin'
+            ]
           },
-          "developers/talawa-api/mutations",
-          "developers/talawa-api/schemas",
-          "developers/talawa-api/authentication",
-          "developers/talawa-api/functionality",
-          "developers/talawa-api/expectations",]
-      },      
-      {
-        "Talawa Admin": ["developers/talawa-admin/talawa-admin-introduction"],
-      }
-    ],    
-
-      "Existing Features": [
-      "functionalities/core-functionalities",
-      "functionalities/plugin-architecture",
-      "functionalities/implementing-plugins",
-      "functionalities/implementing-plugins-example"
-    ],
-    "Desired Features": [
-      "features/features-introduction",
-      {
-        "Admin Portal": ["features/admin-portal/admin-portal-overview"],
-        "Admin Capabilities": [
-          "features/admin-role/admin-role-overview",
-          "features/admin-role/admin-messaging",
-          "features/admin-role/admin-manage-members",
-          "features/admin-role/admin-newsfeed",
-        ],
-        "Chat and Messaging": [
-          "features/messaging-and-chat/messaging-and-chat-overview",
-          "features/messaging-and-chat/message-chat",
-          "features/messaging-and-chat/message-messaging",
-          "features/messaging-and-chat/message-voice-messaging",
-          "features/messaging-and-chat/message-notifications",
-        ],
-        "Coordinator Capabilities": [
-          "features/coordinator-role/coordinator-role-overview",
-        ],
-        Events: [
-          "features/events/event-overview",
-          "features/events/event-create-events",
-          "features/events/event-signup",
-          "features/events/event-grouping",
-          "features/events/event-roles",
-          "features/events/event-attendee-roster",
-          "features/events/event-paid-donation",
-        ],
-        Members: [
-          "features/members/members-overview",
-          "features/members/members-access",
-          "features/members/members-roles",
-          "features/members/members-preapprovals",
-          "features/members/members-registration",
-          "features/members/members-banning",
-          "features/members/members-recovery",
-        ],
-        Donations: [
-          "features/donations/donations-overview",
-          "features/donations/donation-fees",
-        ],
-        "News feed": [
-          "features/news feed/news-feed-overview",
-          "features/news feed/tagging-support",
-          "features/news feed/pinned-posts",
-        ],
-        Notifications: [
-          "features/notifications/notifications-overview",
-          "features/notifications/follow-tag-support",
-        ],
-        "Data Management": [
-          "features/data-management/data-management-overview",
-          "features/data-management/data-management-compression",
-          "features/data-management/data-management-retention",
-          "features/data-management/data-management-storage",
-          "features/data-management/data-management-distribution",
-        ],
-        Organisations: [
-          "features/organisations/organisations-overview",
-          "features/organisations/organisations-onboarding",
-        ],
+          'developers/talawa-api/mutations',
+          'developers/talawa-api/schemas',
+          'developers/talawa-api/authentication',
+          'developers/talawa-api/functionality',
+          'developers/talawa-api/expectations'
+        ]
       },
+      {
+        'Talawa Admin': [
+          'developers/talawa-admin/talawa-admin-introduction',
+
+          {
+            Plugins: [
+              'developers/talawa-admin/plugins/plugin-architecture',
+              'developers/talawa-admin/plugins/implementing-plugins',
+              'developers/talawa-admin/plugins/implementing-plugins-example'
+            ]
+          }
+        ]
+      }
     ],
-    "UI/UX Design": ["design/branding","design/ux/ux-talawa", "design/ux/ux-design-system"],
+    'Requirements Docs': [
+      {
+        'Existing Features': ['functionalities/core-functionalities']
+      },
+      {
+        'Desired Features': [
+          'features/features-introduction',
+          {
+            'Admin Portal': ['features/admin-portal/admin-portal-overview'],
+            'Admin Capabilities': [
+              'features/admin-role/admin-role-overview',
+              'features/admin-role/admin-messaging',
+              'features/admin-role/admin-manage-members',
+              'features/admin-role/admin-newsfeed'
+            ],
+            'Chat and Messaging': [
+              'features/messaging-and-chat/messaging-and-chat-overview',
+              'features/messaging-and-chat/message-chat',
+              'features/messaging-and-chat/message-messaging',
+              'features/messaging-and-chat/message-voice-messaging',
+              'features/messaging-and-chat/message-notifications'
+            ],
+            'Coordinator Capabilities': [
+              'features/coordinator-role/coordinator-role-overview'
+            ],
+            Events: [
+              'features/events/event-overview',
+              'features/events/event-create-events',
+              'features/events/event-signup',
+              'features/events/event-grouping',
+              'features/events/event-roles',
+              'features/events/event-attendee-roster',
+              'features/events/event-paid-donation'
+            ],
+            Members: [
+              'features/members/members-overview',
+              'features/members/members-access',
+              'features/members/members-roles',
+              'features/members/members-preapprovals',
+              'features/members/members-registration',
+              'features/members/members-banning',
+              'features/members/members-recovery'
+            ],
+            Donations: [
+              'features/donations/donations-overview',
+              'features/donations/donation-fees'
+            ],
+            'News feed': [
+              'features/news feed/news-feed-overview',
+              'features/news feed/tagging-support',
+              'features/news feed/pinned-posts'
+            ],
+            Notifications: [
+              'features/notifications/notifications-overview',
+              'features/notifications/follow-tag-support'
+            ],
+            'Data Management': [
+              'features/data-management/data-management-overview',
+              'features/data-management/data-management-compression',
+              'features/data-management/data-management-retention',
+              'features/data-management/data-management-storage',
+              'features/data-management/data-management-distribution'
+            ],
+            Organisations: [
+              'features/organisations/organisations-overview',
+              'features/organisations/organisations-onboarding'
+            ]
+          }
+        ]
+      }
+    ],
+
+    'UI/UX Design': [
+      'design/branding',
+      'design/ux/ux-talawa',
+      'design/ux/ux-design-system'
+    ],
     Internships: [
       {
         Overview: [
-          "internships/internship-ideas",
-          "internships/internship-application-template",
-          "internships/internship-selection-criteria",
+          'internships/internship-ideas',
+          'internships/internship-application-template',
+          'internships/internship-selection-criteria'
         ],
         GSoC: [
-          "internships/gsoc/gsoc-introduction",
-          "internships/gsoc/gsoc-application-process",
+          'internships/gsoc/gsoc-introduction',
+          'internships/gsoc/gsoc-application-process'
         ],
-        GSoD: ["internships/gsod/gsod-ideas"],
-        Palisadoes: ["internships/palisadoes/palisadoes-introduction"],
-      },
-    ],
-  },
-};
+        GSoD: ['internships/gsod/gsod-ideas'],
+        Palisadoes: ['internships/palisadoes/palisadoes-introduction']
+      }
+    ]
+  }
+}
