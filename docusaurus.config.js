13982ccd560ce0f6b165781c8329f8be5e2e617d
wineshuga <uziwinnie@gmail.com>
2023-03-07 13:58:54 +0100

    Remove tutorial text and its heading from footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -82,15 +82,6 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs',
-              },
-            ],
-          },
-          {
             title: 'Community',
             items: [
               {
