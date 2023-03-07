a1664e03a0ceb67812023cc3e0996171ccad8656
akanksha <akanksha.t05@gmail.com>
2023-03-07 22:58:43 +0530

    removed editUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,13 +28,9 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
