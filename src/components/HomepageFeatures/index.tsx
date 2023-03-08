6d957c245460eb81e13832f1c9b552f44ca8c2d6
wineshuga <uziwinnie@gmail.com>
2023-03-08 11:03:43 +0100

    undo text color change

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -48,7 +48,7 @@ function Feature({title, Svg, description}: FeatureItem) {
         <Svg className={styles.featureSvg} role="img" />
       </div>
       <div className="text--center padding-horiz--md">
-        <h3 className={styles.featureTitle}>{title}</h3>
+        <h3>{title}</h3>
         <p>{description}</p>
       </div>
     </div>
