df6663d2d6e3a0bdb8d8da59a102044f918a113c
Patrick Kabuga <kabugap02@gmail.com>
2023-03-11 18:53:26 +0300

    [chore]: Update error page contents

--- src/theme/NotFound.js
+++ src/theme/NotFound.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Translate, {translate} from '@docusaurus/Translate';
 import {PageMetadata} from '@docusaurus/theme-common';
 import Layout from '@theme/Layout';
+import Error404Image from '@site/static/img/errors/404.png'
+import styles from './NotFound.css';
+
 export default function NotFound() {
   return (
     <>
@@ -12,31 +15,33 @@ export default function NotFound() {
         })}
       />
       <Layout>
-        <main className="container margin-vert--xl">
+        <main className="container margin-vert--xl flex-center">
           <div className="row">
             <div className="col col--6 col--offset-3">
+              <img src={Error404Image} class="img-fluid" alt="image" />
               <h1 className="hero__title">
                 <Translate
                   id="theme.NotFound.title"
                   description="The title of the 404 page">
-                  Page Not Found
+                  UH, OH! YOU'RE LOST!
                 </Translate>
               </h1>
               <p>
                 <Translate
-                  id="theme.NotFound.p1"
-                  description="The first paragraph of the 404 page">
-                  We could not find what you were looking for.
-                </Translate>
-              </p>
-              <p>
-                <Translate
                   id="theme.NotFound.p2"
                   description="The 2nd paragraph of the 404 page">
-                  Please contact the owner of the site that linked you to the
-                  original URL and let them know their link is broken.
+                  The page you are looking for does not exist. Check if you have a typo in the URL. You can click the button below to go back to the homepage.
                 </Translate>
               </p>
+              <a href="/" className="flex-center">
+                <button className="btn text-center">
+                  <Translate
+                    id="theme.NotFound.button"
+                    description="Button on the 404 page that redirects to the Homepage">
+                    HOME PAGE
+                  </Translate>
+                </button>
+              </a>
             </div>
           </div>
         </main>
