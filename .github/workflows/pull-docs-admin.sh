6017cf5edb8c76286fea13043afe5d14ffa40d70
gautam-divyanshu <gautamdivyanshu034@gmail.com>
2024-07-04 13:50:59 +0530

    fixed

--- .github/workflows/pull-docs-admin.sh
+++ .github/workflows/pull-docs-admin.sh
@@ -9,7 +9,7 @@ cd talawa-admin
 npm install --global typedoc
 npm install typedoc-plugin-markdown
 npm install --save-dev @types/node
-npx typedoc --entryPoints src/components src/screens --out talawa-admin-docs --plugin typedoc-plugin-markdown --theme markdown --entryPointStrategy expand --exclude "**/*.test.ts" --exclude "**/*.css"
+npx typedoc --entryPoints src/components src/screens --out talawa-admin-docs --plugin typedoc-plugin-markdown --entryPointStrategy expand --exclude "**/*.test.ts" --exclude "**/*.css"
 
 python3 .github/workflows/md_mdx_format_adjuster.py --directory talawa-admin-docs
 
