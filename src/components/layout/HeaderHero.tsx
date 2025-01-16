a935e9c68ba5b9e7cd98b8d4d26f0a83fb11d3ee
Aryan Raj <qwerty4884n@gmail.com>
2025-01-17 04:24:27 +0530

    Add HeaderHero.tsx and index.css

--- src/components/layout/HeaderHero.tsx
+++ src/components/layout/HeaderHero.tsx
@@ -1,38 +1,39 @@
 import React from "react";
-import  Section  from "../../utils/Section";
-import  TwoColumns  from "../../utils/TwoColumns";
-import  HomeCallToAction  from "../../utils/HomeCallToAction";
+import Section from "../../utils/Section";
+import TwoColumns from "../../utils/TwoColumns";
+import HomeCallToAction from "../../utils/HomeCallToAction";
 import useBaseUrl from "@docusaurus/useBaseUrl";
-
 
 function HeaderHero() {
   return (
-    <Section background="light" className="HeaderHero">
-      <div className="socialLinks"></div>
-      <TwoColumns
-        reverse
-        columnOne={
-          <img
-            className="custom-image bounce-animation"
-            src={useBaseUrl("img/image-01.png")}
-            alt="HeaderHero Image"
-          />
-        }
-        columnTwo={
-          <>
-            <h1 className="title">Talawa</h1>
-            <h2 className="tagline">Member Management Software</h2>
-            <p className="description">
-              For open source projects, charities and other non-profits
-            </p>
-            <div className="buttons">
-              <HomeCallToAction />
-            </div>
-          </>
-        }
-      />
-    </Section>
+    <div className="card__container">
+      <Section background="light" className="HeaderHero">
+        <div className="socialLinks"></div>
+        <TwoColumns
+          reverse
+          columnOne={
+            <img
+              className="custom-image bounce-animation"
+              src={useBaseUrl("img/image-01.png")}
+              alt="HeaderHero Image"
+            />
+          }
+          columnTwo={
+            <>
+              <h1 className="title">Talawa</h1>
+              <h2 className="tagline">Member Management Software</h2>
+              <p className="description">
+                For open source projects, charities and other non-profits
+              </p>
+              <div className="buttons">
+                <HomeCallToAction />
+              </div>
+            </>
+          }
+        />
+      </Section>
+    </div>
   );
 }
 
-export default HeaderHero
+export default HeaderHero;
