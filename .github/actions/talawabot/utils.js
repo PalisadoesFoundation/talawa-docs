14b819163461d25e0f73f261db0ebc28bd90359e
VanshikaSabharwal <vanshikasabharwal06@gmail.com>
2025-12-01 13:12:51 +0530

    testing talawa bot

--- .github/actions/talawabot/utils.js
+++ .github/actions/talawabot/utils.js
@@ -1,6 +1,6 @@
 module.exports = {
   sleep: (ms) => new Promise((resolve) => setTimeout(resolve, ms)),
-  THREE_MINUTES: 3 * 60 * 1000,
+  THREE_MINUTES: 3 * 1000,
 
   getUsernamesFromText(body) {
     const matches = body.match(/@([a-zA-Z0-9-_]+)/g) || [];
