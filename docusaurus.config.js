fb8f35c27e0e1e44951f4b5c5efbc79a56ec626d
Peter Harrison <16875803+palisadoes@users.noreply.github.com>
2026-07-28 12:32:29 -0400

    Added support for upcoming v4 changes (#1141)
    * Added support for upcoming v4 changes
    
    * Added support for upcoming v4 changes -2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,6 +15,10 @@ const config = {
       onBrokenMarkdownLinks: 'warn', // Or 'throw', 'ignore'
     },
   },
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
   onBrokenAnchors: 'throw',
   favicon: 'img/favicon.ico',
   deploymentBranch: 'gh-pages',
@@ -179,7 +183,7 @@ const config = {
           },
         ],
         copyright: `Copyright © ${new Date().getFullYear()} The Palisadoes Foundation, LLC. Built with Docusaurus.`,
-      },     
+      },
       prism: {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
